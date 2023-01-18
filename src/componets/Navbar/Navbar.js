e325256617e903ced5439d0a5ea5f64d4dafa75d
Ana Olivero <anagoliverom.2002@gmail.com>
2023-01-18 00:15:17 -0300

    navbar

--- src/componets/Navbar/Navbar.js
+++ src/componets/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () =>{
     return(
 
         <nav style={{display: "flex", justifyContent: "space-around"}}>
-            <h1>Katys Beuaty</h1>
+            <h1>My imports OM</h1>
         <div>
         <Link to='/category/Ofertas' style={{margin:10}}>Ofertas</Link>
         <Link to='/category/Hogar'>Hogar</Link>
--- src/componets/asynMock.js
+++ src/componets/asynMock.js
@@ -1,51 +1,51 @@
 const products =[
     {
     "id":1,
-    "nombre":"",
-    "category": "",
+    "nombre":"Limador de Uñas",
+    "category": "Ofertas",
     "cantidad":1,
     "precio":1200,
-    "img": ""
+    "img": "https://http2.mlstatic.com/D_NQ_NP_687073-MLC52305800899_112022-W.jpg"
     },
     {
     "id":2,
-    "nombre":"",
-    "category": "",
+    "nombre":"Reloj Inteligente",
+    "category": "Ofertas",
     "cantidad":1,
     "precio":1200,
-    "img": ""
+    "img": "https://startechoffice.cl/wp-content/uploads/2022/04/bH.jpg"
     },
     {
     "id":3,
-    "nombre":"",
-    "category": "",
+    "nombre":"Carrito Organizador",
+    "category": "Ofertas",
     "cantidad":1,
     "precio":1400,
-    "img": ""
+    "img": "https://http2.mlstatic.com/D_NQ_NP_734394-MLC47417342111_092021-O.webps"
     },
     {
     "id":4,
-    "nombre":"",
-    "category": "",
+    "nombre":"Aspiradora",
+    "category": "Hogar",
     "cantidad":1,
     "precio":1300,
-    "img": ""
+    "img": "https://http2.mlstatic.com/D_NQ_NP_997735-MLA45272046482_032021-O.jpg"
     } ,
     {
     "id":5,
-    "nombre":"",
-    "category": "",
+    "nombre":"Humificador",
+    "category": "Hogar",
     "cantidad":1,
     "precio":1300,
-    "img": ""
+    "img": "https://http2.mlstatic.com/D_NQ_NP_752726-MLC46939589444_082021-O.webp"
     },
     {
     "id":6,
-    "nombre":"",
-    "category": "",
+    "nombre":"Control Universal",
+    "category": "Hogar",
     "cantidad":1,
     "precio":1300,
-    "img": ""
+    "img": "https://cdnx.jumpseller.com/controlpro/image/14379455/3996_1-01.jpeg?1651016327"
     }   
 ]
 
